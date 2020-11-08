b0e98846b69ed270769a8e1efa2dfef5ecc70b11
4neesc0des <aneeskuar@gmail.com>
2020-11-08 23:39:10 +0530

    new design

---
+++ simple Calculator/javascript/script.js
@@ -0,0 +1,5 @@
+const outputVal = document.querySelector(".output-value").innerHTML;
+console.log(outputVal);
+
+const historyVal = document.querySelector(".history-value").innerHTML;
+console.log(historyVal);
